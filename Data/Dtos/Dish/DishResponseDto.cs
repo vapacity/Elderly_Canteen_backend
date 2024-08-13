6e1d569202905610ebcfd788a5f2c46ac5cf4c60
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-13 19:58:08 +0800

    更新
    1

---
+++ Data/Dtos/Dish/DishResponseDto.cs
@@ -0,0 +1,42 @@
+﻿namespace Elderly_Canteen.Data.Dtos.Dish
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class DishResponseDto
+    {
+        [JsonProperty("msg")]
+        public string Msg { get; set; }
+
+        [JsonProperty("response")]
+        public List<Response> Response { get; set; }
+
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+    }
+
+    public partial class Response
+    {
+        [JsonProperty("category", NullValueHandling = NullValueHandling.Ignore)]
+        public string Category { get; set; }
+
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Id { get; set; }
+
+        [JsonProperty("image", NullValueHandling = NullValueHandling.Ignore)]
+        public string Image { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("price", NullValueHandling = NullValueHandling.Ignore)]
+        public string Price { get; set; }
+
+        [JsonProperty("recipe", NullValueHandling = NullValueHandling.Ignore)]
+        public string Recipe { get; set; }
+    }
+}
---
+++ Data/Repos/DishRepository/IDishRepository.cs
@@ -0,0 +1,9 @@
+﻿using Elderly_Canteen.Data.Entities;
+
+namespace Elderly_Canteen.Data.Repos.DishRepository
+{
+    public interface IDishRepository
+    {
+        Task<IEnumerable<Dish>> GetFormulaOfDishesAsync();
+    }
+}
