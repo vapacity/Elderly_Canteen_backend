89aa819cc7e74db6c31545ffc0d86046b194979d
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-16 17:31:03 +0800

    改了一点personinfo和dishrepository
    dish目前写不了，需要前端修改请求

--- Data/Repos/DishRepository/IDishRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Elderly_Canteen.Data.Entities;
-
-namespace Elderly_Canteen.Data.Repos.DishRepository
-{
-    public interface IDishRepository
-    {
-        Task<IEnumerable<Dish>> GetFormulaOfDishesAsync();
-    }
-}
