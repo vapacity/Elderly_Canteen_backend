476ada3a0ee6c4cfb4504c969da1ce2350c238bd
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-16 18:49:54 +0800

    实现创建购物车api
    如果已经有购物车，返回购物车id，否则创建新的返回

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 // 注册其他服务
 builder.Services.AddScoped<IAccountService, AccountService>();
-
+builder.Services.AddScoped<ICartService, CartService>();
 
 // JWT 身份验证
 var jwtSettings = builder.Configuration.GetSection("Jwt");
