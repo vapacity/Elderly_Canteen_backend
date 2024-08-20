c26e89e445a14a35f086380af473b5a424818b93
vapacity <131332667+vapacity@users.noreply.github.com>
2024-08-20 10:50:49 +0800

    Delete cart api finished
    实现删除购物车api

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using Elderly_Canteen.Services.Implements;
+﻿using Elderly_Canteen.Data.Entities;
+using Elderly_Canteen.Services.Implements;
 using Elderly_Canteen.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,22 +12,40 @@ namespace Elderly_Canteen.Controllers
     public class CartController:ControllerBase
     {
         private readonly ICartService _cartService;
-
+        
         public CartController(ICartService cartService)
         {
             _cartService = cartService;
         }
+
+        private string accountId => User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
         [Authorize]
         [HttpPost("createCart")]
         public async Task<IActionResult> CreateCart()
         {
-            var accountId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
             var cartResponse = await _cartService.CreateCartAsync(accountId);
             if (cartResponse == null)
             {
                 return NotFound();
             }
             if (cartResponse.success == false)
+            {
+                return BadRequest(cartResponse);
+            }
+            return Ok(cartResponse);
+        }
+        [Authorize]
+        [HttpDelete("deleteCart")]
+        public async Task<IActionResult> DeleteCart()
+        {
+            var cartResponse = await _cartService.DeleteCartAsync(accountId);
+            if (cartResponse == null)
+            {
+                return NotFound();
+            }
+            if (!cartResponse.success)
             {
                 return BadRequest(cartResponse);
             }
