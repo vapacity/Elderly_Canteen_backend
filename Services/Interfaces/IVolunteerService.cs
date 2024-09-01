04dd803748d6e7896c877e33bfedb2eac830c69d
359jxf <1151976118@qq.com>
2024-09-01 14:46:16 +0800

    志愿者申请、密码哈希

---
+++ Services/Interfaces/IVolunteerService.cs
@@ -0,0 +1,10 @@
+﻿using Elderly_Canteen.Data.Dtos.Volunteer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elderly_Canteen.Services.Interfaces
+{
+    public interface IVolunteerService
+    {
+        Task ApplyAsync(VolunteerApplicationDto application, string accountId);
+    }
+}
