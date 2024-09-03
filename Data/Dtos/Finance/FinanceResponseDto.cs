038ba7a6baac8bcebf01220f77a881fb6d772b43
Viviennie <143258803+Viviennie@users.noreply.github.com>
2024-09-03 21:22:39 +0800

    财务管理
    根据query获取过滤的财务列表，更新审核状态

---
+++ Data/Dtos/Finance/FinanceResponseDto.cs
@@ -0,0 +1,41 @@
+﻿namespace Elderly_Canteen.Data.Dtos.Finance
+{
+    public class FinanceResponseDto
+    {
+        /// <summary>
+        /// 请求是否成功
+        /// </summary>
+        public bool success { get; set; }
+
+        /// <summary>
+        /// 提示信息
+        /// </summary>
+        public string msg { get; set; }
+
+        /// <summary>
+        /// 员工信息列表
+        /// </summary>
+        public List<FinanceResponseData> response { get; set; }
+    }
+
+    public class FinanceResponseData
+    {
+        public string FinanceId { get; set; } = null!;
+
+        public string FinanceType { get; set; } = null!;
+
+        public DateTime FinanceDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string InOrOut { get; set; } = null!;
+
+        public string AccountId { get; set; } = null!;
+
+        public string? AdministratorId { get; set; }
+
+        public byte[]? Proof { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ Services/Interfaces/IFinanceService.cs
@@ -0,0 +1,14 @@
+﻿using Elderly_Canteen.Data.Dtos.Finance;
+using Elderly_Canteen.Data.Entities;
+
+namespace Elderly_Canteen.Services.Interfaces
+{
+    public interface IFinanceService
+    {
+        //获取财务列表
+        Task<FinanceResponseDto> GetFilteredFinanceInfoAsync(string? financeType = null, string inOrOut = null, string financeDate = null, string financeId = null, string accountId = null);
+        //更新审核状态
+        Task<FinanceResponseDto> UpdateFinanceStatusAsync(string financeId, string status, string administratorId);
+
+    }
+}
