fb08e8f07546f67c2e615c64e9f61b6677f42673
Viviennie <143258803+Viviennie@users.noreply.github.com>
2024-09-04 11:21:11 +0800

    总财务
    1

---
+++ Data/Dtos/Finance/FinanceTotalResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Elderly_Canteen.Data.Dtos.Finance
+{
+    public class FinanceTotalsResponse
+    {
+        public bool success { get; set; }
+        public string msg { get; set; }
+        public FinanceData response { get; set; }
+    }
+
+    public class FinanceData
+    {
+        public decimal NetIn { get; set; }
+        public decimal TotalIn { get; set; }
+        public decimal TotalOut { get; set; }
+    }
+}
